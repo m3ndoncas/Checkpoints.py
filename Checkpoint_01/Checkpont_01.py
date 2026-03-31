1af274ee5ef575726ce95bbbc4f41abebb742b02
m3ndoncas <vitorluizsouza2000@gmail.com>
2026-03-31 20:37:02 -0300

    Initial commit

---
+++ Checkpoint_01/Checkpont_01.py
@@ -0,0 +1,17 @@
+
+from random import randint
+
+secreto = randint(1, 100)
+tentativas = 0
+print("Tente adivinhar o número de 1 a 100")
+while True:
+    userNUmber = int(input("Qual seu palpite? "))
+    if userNUmber == secreto:
+        print(f"Você acertou em {tentativas} tentativas!!!")
+        break
+    elif userNUmber > secreto:
+        print("Vish o número é um pouco maior...")
+        tentativas += 1
+    elif userNUmber < secreto:
+        print("Vish o número é um pouco maior...")
+        tentativas += 1
